9ffa57c0696184b6c841df44fdeb25e768b2277b
Tyler Wenzel <Tyler@tylers-mbp.home>
2021-02-28 15:22:55 -0500

    RPS Bonus features 1 & 3

--- lesson_2/RPS.js
+++ lesson_2/RPS.js
@@ -1,23 +1,44 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
-
+let userWins = 0;
+let computerWins = 0;
+// eslint-disable-next-line complexity
 function displayWinner(choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
+  if ((choice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
+      (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
+      (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
+      (choice === 'lizard' && (computerChoice === 'spock' || computerChoice === 'paper')) ||
+      (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard'))) {
     prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-             (choice === 'paper' && computerChoice === 'scissors') ||
-             (choice === 'scissors' && computerChoice === 'rock')) {
+    userWins += 1;
+  } else if ((computerChoice === 'rock' && (choice === 'scissors' || choice === 'lizard')) ||
+  (computerChoice === 'paper' && (choice === 'rock' || choice === 'spock')) ||
+  (computerChoice === 'scissors' && (choice === 'paper' || choice === 'lizard')) ||
+  (computerChoice === 'lizard' && (choice === 'spock' || choice === 'paper')) ||
+  (computerChoice === 'scissors' && (choice === 'paper' || choice === 'lizard'))) {
     prompt('Computer wins!');
+    computerWins += 1;
   } else {
     prompt("It's a tie!");
+  }
+}
+function displayRecords(user , computer) {
+  if (user < 4 && computer < 4) {
+    console.log(`You have ${user} wins and your opponent has ${computer} wins`);
+    if (computer === 3) {
+      console.log("Your opponent has won the series!");
+    } else if (user ===   3) {
+      console.log('You have won the series!');
+    }
+  } else {
+    computer = 0;
+    user = 0;
   }
 }
 
@@ -29,11 +50,11 @@ while (true) {
     prompt("That's not a valid choice");
     choice = readline.question();
   }
-
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
   displayWinner(choice, computerChoice);
+  displayRecords(userWins, computerWins);
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
